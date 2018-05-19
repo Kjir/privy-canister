9bccd9266ab9e3998bf0cbb3100198a48710f911
Stéphane Bisinger <stephane.bisinger@protonmail.com>
2018-05-19 21:26:10 +0200

    Create an example Python/Quart application

---
+++ todo-quart/todo.py
@@ -0,0 +1,35 @@
+from quart import Quart, jsonify, request
+from quart.helpers import make_response
+
+app = Quart(__name__)
+
+@app.route('/todos')
+async def todos():
+    resp = await make_response(jsonify({
+        'todos': [
+            { 'item': 'first' },
+            { 'item': 'secondpy' }
+        ]
+    }))
+
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+TODO_CORS_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST',
+    'Access-Control-Allow-Headers': 'content-type'
+}
+
+@app.route('/todo/item', methods=['POST'], provide_automatic_options=False)
+async def todo():
+    item = await request.get_json()
+    resp = await make_response(jsonify(item=item['item']))
+    resp.headers.update(TODO_CORS_HEADERS)
+    return resp
+
+@app.route('/todo/item', methods=['OPTIONS'], provide_automatic_options=False)
+async def todo_options():
+    return '', TODO_CORS_HEADERS
+
+app.run(host='0.0.0.0')
